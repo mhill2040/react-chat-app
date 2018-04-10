d1e95d432a1d6bac86b79910523d5117ab25858d
Melissa Hill <mhill2040@gmail.com>
2018-04-10 11:39:12 -0600

    chat app

---
+++ client/src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import NoMatch from './NoMatch';
+import NavBar from './NavBar';
+import Login from './Login';
+import Register from './Register';
+import Flash from './Flash';
+import Home from './Home';
+import ChatWindow from './ChatWindow';
+import ProtectedRoute from './ProtectedRoute';
+import { Switch, Route } from 'react-router-dom';
+import FetchUser from './FetchUser';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Flash />
+        <FetchUser>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+            <ProtectedRoute exact path='/chat' component={ChatWindow} />
+            <Route component={NoMatch} />
+          </Switch>
+        </FetchUser>
+      </div>
+    );
+  }
+}
+
+export default App;
